e326d4f87ac491e6c3f53c955c2e80ab0c10d67c
Chirag Tagadiya <46391124+ChiragTagadiya@users.noreply.github.com>
2022-04-21 08:22:05 +0530

    Create App.js
    Initial file commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import ProductList from './src/ProductList';
+
+const App = () => {
+
+  return (
+    <SafeAreaView>
+      <ProductList />
+    </SafeAreaView>
+  );
+};
+
+export default App;
